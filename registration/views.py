12ee88c305d7c0574a3ee75e68ffdb241e0efcc8
Aayush Chhuka <aayushchhuka7@gmail.com>
2026-02-05 13:59:35 +0545

    initial setup for registration

---
+++ registration/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import redirect, render
+
+from registration.forms import RegistrationForm
+
+
+def registration_form(request):
+    """Handle registration form display and submission"""
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+
+        if form.is_valid():
+            print("Form is valid")
+            return redirect('registration:form')
+    else:
+        form = RegistrationForm()
+
+    return render(request, 'registration/form.html', {'form': form})
