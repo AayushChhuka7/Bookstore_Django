a61e9cc079b74e09a09dbd00d62835c9eae98ade
Aayush Chhuka <aayushchhuka7@gmail.com>
2026-02-05 14:21:44 +0545

    Form

--- registration/views.py
+++ registration/views.py
@@ -1,17 +1,22 @@
-from django.shortcuts import redirect, render
-
-from registration.forms import RegistrationForm
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from .models import Registration
 
 
 def registration_form(request):
-    # """Handle registration form display and submission"""
-    # if request.method == 'POST':
-    #     form = RegistrationForm(request.POST)
+    """Handle registration form display and submission"""
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
 
-    #     if form.is_valid():
-    #         print("Form is valid")
-    #         return redirect('registration:form')
-    # else:
-    form = RegistrationForm()
+        if form.is_valid():
+            # Get cleaned data
+            print("success")
+            return render(request, 'registration/form.html',
+                          {
+                              'form':form,
+                              "success":"Successful"
+                          })
+    else:
+        form = RegistrationForm()
 
     return render(request, 'registration/form.html', {'form': form})
