709c491dc61d2c5fb5050fb472490f1702a2b8b4
Aayush Chhuka <aayushchhuka7@gmail.com>
2026-02-05 13:24:36 +0545

    Adding pages for books

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,5 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+
+from .models import Book
 
 # Create your views here.
 def index(request):
-    return render(request,"book_outlet/index.html")
+    books=Book.objects.all()
+    return render(request,"book_outlet/index.html",{
+        "books":books
+    })
+def book_detail(request, id):
+  book = get_object_or_404(Book, pk=id)
+  return render(request, "book_outlet/book_detail.html", {
+    "book": book
+  })
